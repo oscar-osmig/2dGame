3254e36379d3cfa31279962ccb1d4f37a4cea87e
Osmig_Torres <torresosmig@gmail.com>
2024-10-21 08:00:43 -0700

    workedkey handler and dummy player moves well

--- game/src/main/java/com/osmig/GamePanel.java
+++ game/src/main/java/com/osmig/GamePanel.java
@@ -16,7 +16,16 @@ public class GamePanel extends JPanel implements Runnable{
     final  int screenWidth = tileSize * maxTileCol; // 768px
     final int screenHeight = tileSize * getMaxTileRow; // 576px
 
+    // FPS
+    int FPS = 60;
+
+    KeyHandler keyh = new KeyHandler();
     Thread gameThread;
+
+    // set players default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     // constructor
     public  GamePanel(){
@@ -24,6 +33,9 @@ public class GamePanel extends JPanel implements Runnable{
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyh);
+        this.setFocusable(true);
+
     }
     public  void  startGameThread(){
         gameThread = new Thread(this);
@@ -31,19 +43,40 @@ public class GamePanel extends JPanel implements Runnable{
     }
     @Override
     public void run() {
+        double drawInterval = 1000000000/FPS; // 0.01666 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
         while(gameThread != null){ // game loop
-            //System.out.println("the game loop is running");
-            // 1. UPDATE: update information such as character position
-            update();
 
-            repaint();
+            update();  // 1. UPDATE: update information such as character position
 
+            repaint(); // 2. DRAW: draw the screen  with updated information
 
-            // 2. DRAW: draw the screen  with updated information
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000; // sleep takes in milliseconds, so we converted to it to avoid issues
+                if (remainingTime < 0){
+                    remainingTime = 0;
+                }
+                Thread.sleep((long)remainingTime);
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
         }
     }
 
     public void update(){
+        if(keyh.upPressed == true){
+            playerY -= playerSpeed;
+        } else if (keyh.downPressed == true) {
+            playerY += playerSpeed;
+        } else if (keyh.leftPressed == true) {
+            playerX -= playerSpeed;
+        } else if (keyh.rightPressed == true) {
+            playerX += playerSpeed;
+        }
 
     }
 
@@ -53,7 +86,7 @@ public class GamePanel extends JPanel implements Runnable{
         Graphics2D g2 = (Graphics2D)g;
 
         g2.setColor(Color.white);
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         g2.dispose();
 
